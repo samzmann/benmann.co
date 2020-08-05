27ecf84dd6827d1d30032c71f44407efa2f848dd
Samuel Mann <samuelcmann@gmail.com>
2020-08-05 12:11:55 +0200

    added main pages

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import SEO from '../components/seo'
+import Layout from '../components/layout'
 import { graphql, useStaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
 
-export const TitleHeader = () => {
+const ProjectsPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allFile(filter: { relativePath: { eq: "skyline.jpg" } }) {
+      allFile(filter: { relativePath: { eq: "projects.jpg" } }) {
         edges {
           node {
             childImageSharp {
@@ -18,21 +20,16 @@ export const TitleHeader = () => {
       }
     }
   `)
-
   return (
-    <div className="text-center">
+    <Layout>
+      <SEO title="Projects" />
       <Img
         className="h-32 sm:h-64"
         fluid={data.allFile.edges[0].node.childImageSharp.fluid}
       />
-      <h1 className="text-5xl p-8 md:pt-16 md:pb-8">Hi, I'm Ben 👋</h1>
-      <h3 className="text-xl">
-        Welcome to my website, all about{' '}
-        <span className="font-semibold border-b-2 text-indigo-500 bg-indigo-100 p-2 rounded">
-          urban planning
-        </span>{' '}
-        and some other passions of mine. Enjoy!
-      </h3>
-    </div>
+      <h1>Projects</h1>
+    </Layout>
   )
 }
+
+export default ProjectsPage
--- src/pages/travelblog.js
+++ src/pages/travelblog.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import SEO from '../components/seo'
+import Layout from '../components/layout'
 import { graphql, useStaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
 
-export const TitleHeader = () => {
+const TravelBlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allFile(filter: { relativePath: { eq: "skyline.jpg" } }) {
+      allFile(filter: { relativePath: { eq: "travel.jpg" } }) {
         edges {
           node {
             childImageSharp {
@@ -18,21 +20,16 @@ export const TitleHeader = () => {
       }
     }
   `)
-
   return (
-    <div className="text-center">
+    <Layout>
+      <SEO title="Travel Blog" />
       <Img
         className="h-32 sm:h-64"
         fluid={data.allFile.edges[0].node.childImageSharp.fluid}
       />
-      <h1 className="text-5xl p-8 md:pt-16 md:pb-8">Hi, I'm Ben 👋</h1>
-      <h3 className="text-xl">
-        Welcome to my website, all about{' '}
-        <span className="font-semibold border-b-2 text-indigo-500 bg-indigo-100 p-2 rounded">
-          urban planning
-        </span>{' '}
-        and some other passions of mine. Enjoy!
-      </h3>
-    </div>
+      <h1>Travel Blog</h1>
+    </Layout>
   )
 }
+
+export default TravelBlogPage
